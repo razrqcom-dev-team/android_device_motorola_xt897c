37c9bc530168e554c370ec4ac15942a441583a89
dhacker29 <davidhackerdvm@gmail.com>
2013-09-17 22:37:36 -0500

    Refactor for qcom-common
    Change-Id: I8b418c2dfe0eae27b68019c3702d299bf22e9d67

--- device_xt897c.mk
+++ device_xt897c.mk
@@ -91,6 +91,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf
 
-$(call inherit-product, device/motorola/msm8960-common/modules/nfc/nfc.mk)
+$(call inherit-product, device/motorola/qcom-common/modules/nfc/nfc.mk)
 $(call inherit-product, vendor/motorola/xt897/xt897-vendor.mk)
 
